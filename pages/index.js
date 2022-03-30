419bcd9866ed81fb5d75044f859b1c2cb3fbe56f
developer833 <byronpantoja@gmail.com>
2022-03-30 08:42:06 +0800

    adding some styling

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,11 @@ import sectionsData from 'data/sectionsData';
 export default function Home() {
   const list = sectionsData.map((l) => (
     <ul key={l[0]}>
-      <h1 className='font-bold text-xl'>
+      <h1 className='mx-auto font-extrabold text-2xl my-5 uppercase'>
         {l[0]}
       </h1>
       {l[1].map((i) => (
-        <Link
-          key={i.id}
-          href={`/answers/${i.url}`}
-        >
+        <Link key={i.id} href={i.url}>
           <a>
             <h3>{i.title}</h3>
           </a>
@@ -30,8 +27,12 @@ export default function Home() {
         </title>
       </Head>
       <main>
-        <h1>Home</h1>
-        <div>{list}</div>
+        <div className='max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
+          <h2 className='text-5xl font-extrabold tracking-tight text-gray-900 mb-5'>
+            Wordscape833 - Answers
+          </h2>
+          <div>{list}</div>
+        </div>
       </main>
     </>
   );
